e9768d0753bb1184ac7353c07f41195aeff2d9a1
Petr Sedláček <petr.spelos@gmail.com>
2018-05-05 16:14:22 +0200

    Add Tests project
    Added Unit Test project

---
+++ JustineCore.Tests/Helpers/InMemoryDataStorage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JustineCore.Storage;
+
+namespace JustineCore.Tests.Helpers
+{
+    public class InMemoryDataStorage : IDataStorage
+    {
+        private readonly Dictionary<string, object> _storage;
+
+        public InMemoryDataStorage()
+        {
+            _storage = new Dictionary<string, object>();
+        }
+
+        public void StoreObject(object obj, string key)
+        {
+            if (_storage.ContainsKey(key))
+                _storage[key] = obj;
+            else
+                _storage.Add(key, obj);
+        }
+
+        public T RestoreObject<T>(string key)
+        {
+            if (_storage.ContainsKey(key))
+                return (T)_storage[key];
+
+            throw new DataStorageKeyDoesNotExistException(
+                $"No object stored with the '{key}' key."
+            );
+        }
+    }
+}
---
+++ JustineCore.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace JustineCore.Tests
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.Pass();
+        }
+    }
+}
