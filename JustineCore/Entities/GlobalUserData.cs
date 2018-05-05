10d40197e42a4a6680b2ebda2b574b550bc664da
Petr Sedláček <petr.spelos@gmail.com>
2018-05-05 18:12:23 +0200

    Add GlobalUserData
    Added entity for GlobalUserData.

---
+++ JustineCore/Entities/GlobalUserData.cs
@@ -0,0 +1,8 @@
+﻿namespace JustineCore.Entities
+{
+    public class GlobalUserData
+    {
+        public ulong DiscordId { get; set; }
+        public DataConsent CollectionConsent { get; set; }
+    }
+}
