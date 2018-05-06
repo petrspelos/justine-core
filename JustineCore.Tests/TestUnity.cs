964add7bdc97814558157d1990e65e3165f6393e
Petr Sedláček <petr.spelos@gmail.com>
2018-05-06 10:10:00 +0200

    Add Unity for Tests
    Added a TestUnity class for unity with dummy types.

---
+++ JustineCore.Tests/TestUnity.cs
@@ -0,0 +1,33 @@
+﻿using JustineCore.Storage;
+using JustineCore.Tests.Helpers;
+using Unity;
+using Unity.Resolution;
+
+namespace JustineCore.Tests
+{
+    internal static class TestUnity
+    {
+        private static UnityContainer _container;
+
+        public static UnityContainer Container
+        {
+            get
+            {
+                if (_container == null)
+                    RegisterTypes();
+                return _container;
+            }
+        }
+
+        public static void RegisterTypes()
+        {
+            _container = new UnityContainer();
+            _container.RegisterType<IDataStorage, InMemoryDataStorage>();
+        }
+
+        public static T Resolve<T>()
+        {
+            return (T)Container.Resolve(typeof(T), string.Empty, new CompositeResolverOverride());
+        }
+    }
+}
