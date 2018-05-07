b88105dad31e55567443bc251e1613a5e5db997e
Petr Sedláček <petr.spelos@gmail.com>
2018-05-07 21:34:03 +0200

    Separate command execution from condition checking
    Created new method TryRunAsBotCommand to better separate the command validation logic from command execution in CommandHandler.cs.

--- JustineCore/Discord/Handlers/CommandHandler.cs
+++ JustineCore/Discord/Handlers/CommandHandler.cs
@@ -25,19 +25,24 @@ namespace JustineCore.Discord.Handlers
 
             var context = new SocketCommandContext(_client, msg);
             if (context.User.IsBot) return;
-
+            
             var argPos = 0;
             if (msg.HasMentionPrefix(_client.CurrentUser, ref argPos))
             {
-                var cmdSearchResult = _commandService.Search(context, argPos);
-                if (cmdSearchResult.Commands.Count == 0) return;
+                await TryRunAsBotCommand(context, argPos);
+            }
+        }
 
-                var result = await _commandService.ExecuteAsync(context, argPos);
+        private async Task TryRunAsBotCommand(SocketCommandContext context, int argPos)
+        {
+            var cmdSearchResult = _commandService.Search(context, argPos);
+            if (cmdSearchResult.Commands.Count == 0) return;
 
-                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
-                {
-                    await context.Channel.SendMessageAsync($"{context.User.Mention}\n:negative_squared_cross_mark: **I couldn't complete the operation**\nApparently, {result.ErrorReason}");
-                }
+            var result = await _commandService.ExecuteAsync(context, argPos);
+
+            if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+            {
+                await context.Channel.SendMessageAsync($"{context.User.Mention}\n:negative_squared_cross_mark: **I couldn't complete the operation**\nApparently, {result.ErrorReason}");
             }
         }
     }
