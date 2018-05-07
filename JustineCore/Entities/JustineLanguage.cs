9ad7233aeea35c8603a143129bece06e37e2b1ec
Petr Sedláček <petr.spelos@gmail.com>
2018-05-07 20:12:45 +0200

    Add Localization
    Implemented Localization and base english.json.

---
+++ JustineCore/Entities/JustineLanguage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace JustineCore.Entities
+{
+    public class JustineLanguage
+    {
+        public uint LanguageId { get; set; }
+        public string LanguageName { get; set; }
+        public Dictionary<string, string> Resources { get; set; }
+    }
+}
