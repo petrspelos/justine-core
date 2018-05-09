0d75701c1fc624f5f3adf112e95a1604d0a4a69a
Petr Sedláček <petr.spelos@gmail.com>
2018-05-09 17:17:03 +0200

    Add Exception file for DataStorage and Localizations
    Moved Exceptions into a separate file.

---
+++ JustineCore/Storage/Exceptions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace JustineCore.Storage
+{
+    public class DataStorageKeyDoesNotExistException : Exception
+    {
+        public DataStorageKeyDoesNotExistException()
+        {
+        }
+
+        public DataStorageKeyDoesNotExistException(string message)
+            : base(message)
+        {
+        }
+
+        public DataStorageKeyDoesNotExistException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+    public class DataStorageGroupDoesNotExistException : Exception
+    {
+        public DataStorageGroupDoesNotExistException()
+        {
+        }
+
+        public DataStorageGroupDoesNotExistException(string message)
+            : base(message)
+        {
+        }
+
+        public DataStorageGroupDoesNotExistException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+    internal class LanguageNotFoundException : Exception
+    {
+        public LanguageNotFoundException()
+        {
+        }
+
+        public LanguageNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public LanguageNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+    internal class LanguageResourceKeyNotFoundException : Exception
+    {
+        public LanguageResourceKeyNotFoundException()
+        {
+        }
+
+        public LanguageResourceKeyNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public LanguageResourceKeyNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
