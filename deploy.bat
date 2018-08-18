dab349f9afebe7084fb075c9c5e78f37081a7e59
Petr Sedláček <petr.spelos@gmail.com>
2018-08-18 16:22:11 +0200

    Fix deploy script issue

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 cd ~/NetCore/justine-core/src/JustineCore
 dotnet publish -r win-arm -c Release
 ssh Administrator@192.168.0.101 "kill.exe JustineCore"
-scp -r ~/NetCore/justine-core/src/JustineCore/bin/Release/netcoreapp2.0/win-arm Administrator@192.168.0.101:./../../../justine-core
+scp -r ~/NetCore/justine-core/src/JustineCore/bin/Release/netcoreapp2.0/win-arm/publish/* Administrator@192.168.0.101:./../../../justine-core
 ssh Administrator@192.168.0.101 "shutdown /r /t 0"
